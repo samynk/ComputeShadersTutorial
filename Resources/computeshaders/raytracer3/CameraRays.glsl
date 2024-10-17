eefc0b625ea1bad2fbc4ba9f05708db0accf61ba
Koen Samyn <samyn.koen@gmail.com>
2024-10-17 11:40:34 +0200

    changed the local size
    TODO : think about possible configuration of the local sizes.

--- Resources/computeshaders/raytracer3/CameraRays.glsl
+++ Resources/computeshaders/raytracer3/CameraRays.glsl
@@ -1,6 +1,6 @@
 #version 430 core
 
-layout (local_size_x = 8, local_size_y = 8, local_size_z = 1) in;
+layout (local_size_x = 16, local_size_y = 16, local_size_z = 1) in;
 
 uniform float fieldOfView = 1;
 uniform float maxT = 1000;
