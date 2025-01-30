8ae4deb7d8f8b61606a169e777f1886918641e3d
Koen Samyn <samyn.koen@gmail.com>
2025-01-30 12:07:01 +0100

    defined all the separate projects.

--- dotnet/Program.cs
+++ dotnet/Program.cs
@@ -9,53 +9,15 @@ namespace ComputeShaderTutorial
     {
         static void Main(string[] args)
         {
+            Project01 window = new Project01("Postprocessing", 512, 512);
+            //Project02 window = new Project02("Convert to texture", 1024, 1024);
+            //Project03 window = new Project03("Conway", 1024, 1024);
             //Project04 window = new Project04("Boids", 1024, 1024);
             //Project05 window = new Project05("Camera rays",512,512);
             //Project06 window = new Project06("Sphere scene", 512, 512);
             //Project07 window = new Project07("Sphere scene with camera", 512, 512);
-            Project08 window = new Project08("Sphere scene with bounces", 1024,1024);
+            //Project08 window = new Project08("Sphere scene with bounces", 1024,1024);
             window.Run();
         }
     }
-
-    public class ComputeShaderWindow : GameWindow
-    {
-        SurfaceRenderer renderer;
-        GLImage inputImage;
-        ComputeShader grayScale;
-        public ComputeShaderWindow(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings)
-            : base(gameWindowSettings, nativeWindowSettings)
-        {
-            renderer = new SurfaceRenderer(0, base.ClientSize.X, base.ClientSize.Y,
-               "Resources/shaders/fullscreen_quad.vert",
-               "Resources/shaders/fullscreen_quad.frag");
-
-            inputImage = new GLImage(0, TextureAccess.ReadOnly, "Resources/computeshaders/input.png");
-            grayScale = new ComputeShader("Resources/computeshaders/imageprocessing/grayscale.glsl");
-        }
-
-        protected override void OnLoad()
-        {
-            base.OnLoad();
-            renderer.Init();
-            inputImage.Init();
-            grayScale.Compile();
-        }
-
-        protected override void OnRenderFrame(FrameEventArgs args)
-        {
-            base.OnRenderFrame(args);
-
-            // Dispatch compute work and draw results here
-            grayScale.Use();
-            inputImage.BindAsCompute(0);
-            renderer.BindAsCompute(1);
-            grayScale.Compute(renderer._width, renderer._height);
-            
-            renderer.DrawQuadWithTexture();
-
-            SwapBuffers();
-        }
-    }
 }
-
---
+++ dotnet/Project01.cs
@@ -0,0 +1,40 @@
+﻿using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputeShaderTutorial
+{
+    internal class Project01 : ComputeWindow
+    {
+        GLImage inputImage;
+        ComputeShader grayScale;
+
+        private const int scaleFactor = 4;
+        public Project01(String title, int w, int h)
+            : base(title,w,h)
+        {
+            inputImage = new GLImage(0, TextureAccess.ReadOnly, "Resources/computeshaders/input.png");
+            grayScale = new ComputeShader("Resources/computeshaders/imageprocessing/grayscale.glsl");
+        }
+
+        protected override void Init()
+        {
+            inputImage.Init();
+            grayScale.Compile();
+        }
+
+        protected override void Compute()
+        {
+            grayScale.Use();
+            inputImage.BindAsCompute(0);
+            BindAsCompute(1);
+            grayScale.Compute(GetClientWidth(), GetClientHeight());
+            GL.MemoryBarrier(MemoryBarrierFlags.AllBarrierBits);
+        }
+    }
+}
