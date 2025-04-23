30611b6f5507e935273edf93af40390b2018fdfa
Koen Samyn <samyn.koen@gmail.com>
2025-04-23 15:25:58 +0200

    Fixed upside down rendering
    Thank you Matheo G.

--- dotnet/SurfaceRenderer.cs
+++ dotnet/SurfaceRenderer.cs
@@ -14,14 +14,14 @@ namespace ComputeShaderTutorial
         {
             //   Positions     TexCoords
             // Triangle 1
-            -1.0f,  1.0f,     0.0f, 1.0f,
-            -1.0f, -1.0f,     0.0f, 0.0f,
-             1.0f,  1.0f,     1.0f, 1.0f,
+            -1.0f,  1.0f,     0.0f, 0.0f, // bottom left
+            -1.0f, -1.0f,     0.0f, 1.0f, // top left
+             1.0f,  1.0f,     1.0f, 0.0f, // bottom right
 
             // Triangle 2
-            -1.0f, -1.0f,     0.0f, 0.0f,
-             1.0f, -1.0f,     1.0f, 0.0f,
-             1.0f,  1.0f,     1.0f, 1.0f
+            -1.0f, -1.0f,     0.0f, 1.0f, // top left
+             1.0f, -1.0f,     1.0f, 1.0f, // top right
+             1.0f,  1.0f,     1.0f, 0.0f  // bottom right
         };
 
         private readonly string _vertexShaderFile;
